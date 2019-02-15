2442b51aac04bc47844279ee00f23a23dfbcbabe
lingchuanA <875321727@qq.com>
2019-02-15 22:56:56 +0800

    about linked-list

--- easy/Solution.java
+++ easy/Solution.java
@@ -36,9 +36,15 @@ public class Solution {
         throw new IllegalArgumentException("No two sum is target");
     }
 
-    //第2题 移除链表元素
+    /**
+     * 移除链表元素
+     * @param head
+     * @param val
+     * @return
+     */
     public ListNode removeElements(ListNode head, int val) {
-        while(head.val == val&&head != null) head = head.next;
+        if(head == null) return null;
+        while(head != null&&head.val == val) head = head.next;
         if(head == null) return null;
         ListNode p;
         p = head;
@@ -53,6 +59,17 @@ public class Solution {
         return head;
     }
 
+    /**
+     * 删除链表中的结点
+     * @date 2019/2/15
+     * node是链表的一部分，且node不在链表尾
+     * 因为无法直接删去node结点，将node下一结点的值复制给node，删除node的下一结点
+     * @param node
+     */
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
 
 
     //第20题
