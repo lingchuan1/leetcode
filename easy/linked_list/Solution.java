c50997d91b84c96171c66972bc5df4585d5efe6d
lingchuanA <875321727@qq.com>
2019-02-22 17:33:19 +0800

    mergeList

--- easy/linked_list/Solution.java
+++ easy/linked_list/Solution.java
@@ -57,6 +57,44 @@ public class Solution {
         }
         return head;
     }
+
+    /**
+     * 合并两个有序链表
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode l3 = null,p,node = null;
+        while(l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                p = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                p = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            if (node == null){
+                node = p;
+                l3 = node;
+            }
+            else {
+                node.next = p;
+                node = p;
+            }
+        }
+        if(node != null){
+            if(l1 == null )
+                node.next = l2;
+            if(l2 == null)
+                node.next = l1;
+        }
+        else {
+            node = (l1 == null?l2:l1);
+            l3 = node;
+        }
+        return l3;
+    }
 }
 class ListNode {
     int val;
